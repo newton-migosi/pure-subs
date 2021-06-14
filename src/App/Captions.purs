b37dd4278e3d0f35aa6b90eb6e1fb629afe6a965
Newton Migosi <newtonmigosi@gmail.com>
2021-06-14 14:10:55 +0300

    ✨ integrated video component to respond to video loading

--- src/App/Captions.purs
+++ src/App/Captions.purs
@@ -2,17 +2,17 @@ module App.Captions (component) where
 
 import Prelude
 
+import App.Styles as Styles
 import App.Video as Video
+import Effect.Aff.Class (class MonadAff)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.CSS as CSS
 import Halogen.HTML.Events as HE
-import Web.UIEvent.KeyboardEvent as Ev
 import Halogen.HTML.Properties as HP
-import Web.HTML.Common (AttrName(..))
-import Data.Maybe (Maybe(..))
-import Web.Event.Event as Event
 import Type.Proxy (Proxy(..))
-import Effect.Aff.Class (class MonadAff)
+import Web.HTML.Common (AttrName(..))
+import Web.UIEvent.KeyboardEvent as Ev
 
 
 type Slots = 
@@ -28,7 +28,7 @@ data Action =
   | HandleInput Ev.KeyboardEvent
 
 type State =
-  { captions :: Array String }
+  { captions :: Array String, video_selected :: Boolean }
 
 component :: forall q i o m. MonadAff m => H.Component q i o m
 component =
@@ -40,27 +40,38 @@ component =
 
 initialState :: forall i. i -> State
 initialState _ =
-  { captions: ["First caption", "Second caption", "Third caption"]
+  { captions: ["First caption"]
+  , video_selected: false
   }
 
 render :: forall m. MonadAff m => State -> H.ComponentHTML Action Slots m
 render state =
   HH.div_
-    [ HH.h1_ [HH.text "Add Captions"] 
-    , HH.slot _video_player unit Video.component unit HandleVideo 
-    , HH.div_ (map caption state.captions)
+    [ HH.div 
+        [ CSS.style Styles.header ]
+        [ HH.h1_ [HH.text "Add Captions"] ] 
+    , HH.div
+        [ CSS.style Styles.video_container ] 
+        [ HH.slot _video_player unit Video.component unit HandleVideo ]
+    , HH.div
+        [ CSS.style Styles.captions_container ]
+        [ if state.video_selected 
+          then HH.div_ (map caption state.captions) 
+          else HH.span_ [ HH.text "" ]
+        ]
     ]
 
+caption :: forall m. MonadAff m => String -> H.ComponentHTML Action Slots m
 caption txt = 
   HH.div 
     [ HP.attr (AttrName "contenteditable") "true"
     , HE.onKeyUp HandleInput
+    , CSS.style Styles.caption
     ] 
-    [ HH.text txt
-    ]
+    [ HH.text txt ]
 
 handleAction ::forall o m slots. Action -> H.HalogenM State Action slots o m Unit
 handleAction = case _ of
   CaptionComplete -> pure unit
-  HandleVideo Video.VideoLoaded -> pure unit
+  HandleVideo Video.VideoLoaded -> H.modify_ (\s -> s {video_selected = true})
   HandleInput ev -> pure unit
